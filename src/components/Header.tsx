e84c7be1eaf3fcf7e4c82bea6507728f075c8ec8
neo2001-cross <ngochuy11010@gmail.com>
2023-03-07 21:18:18 +0700

    First commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import './Header.css'
+
+function Header() {
+    return(
+        <div className='header'>
+            <a href="#" className='headerName'>Neo Paradox</a>
+            <a href="#">Works</a>
+            <a href="#">Technologies</a>
+        </div>
+    )
+}
+
+export default Header
